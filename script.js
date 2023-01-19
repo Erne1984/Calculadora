18339a2805d8a9090066cd9cb4fb32b7d3220bc2
Ernesto Floriano Amorim <ernestofamorim@gmail.com>
2023-01-19 11:44:32 -0300

    função para inserir números inserida

--- script.js
+++ script.js
@@ -9,19 +9,36 @@ const previousOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
-
+   
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement;
       this.currentOperandTextElement = currentOperandTextElement;
+      this.clear();
     }
 
+    chooseOperation(operation) {
+        this.operation = operation;
+
+        this.previousOperand = `${this.currentOperand} ${this.operation}`
+        this.currentOperand = "";
+    }
+
+
+    // função para digitar os números
+    appendNumbeer(number) {
+        if (this.currentOperand.includes('.') && number === '.') return;
+        this.currentOperand = `${this.currentOperand}${number.toString()}`
+    }
+
+    //limpa o valor das variaveis
     clear() {
         this.currentOperand = "";
         this.previousOperand = "";
         this.operation = undefined;
     }
 
+    // atualiza os dados digitados na tela
     updtadeDisplay() {
         this.previousOperandTextElement.innerText = this.previousOperand;
         this.currentOperandTextElement.innerText = this.currentOperand;
@@ -29,7 +46,24 @@ class Calculator {
 
 }
 
+//instaciando o objeto e chamando as funções
+
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+// passa o valor do parâmetro do número digitado para o appendNumber
+for (const numberButton of numberButtons) {
+    numberButton.addEventListener('click', () => {
+        calculator.appendNumbeer(numberButton.innerText);
+        calculator.updtadeDisplay();
+    })
+}
+
+for (const operationButton of operationButtons) {
+    operationButton.addEventListener('click', () => {
+        calculator.chooseOperation(operationButton.innerText)
+        calculator.updtadeDisplay();
+    })
+}
 
 allClearButton.addEventListener('click', () => {
     calculator.clear();
